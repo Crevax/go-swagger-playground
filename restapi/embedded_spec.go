f97a308cde5975b5c7d352c771c70e5997dba309
CJ Davis <cj@cjdavis.me>
2019-01-20 19:26:17 -0500

    Add schema/code for creating a todo item

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -72,6 +72,35 @@ func init() {
             }
           }
         }
+      },
+      "post": {
+        "tags": [
+          "todos"
+        ],
+        "operationId": "addOne",
+        "parameters": [
+          {
+            "name": "body",
+            "in": "body",
+            "schema": {
+              "$ref": "#/definitions/item"
+            }
+          }
+        ],
+        "responses": {
+          "201": {
+            "description": "created",
+            "schema": {
+              "$ref": "#/definitions/item"
+            }
+          },
+          "default": {
+            "description": "generic error response",
+            "schema": {
+              "$ref": "#/definitions/error"
+            }
+          }
+        }
       }
     }
   },
@@ -159,6 +188,35 @@ func init() {
               "items": {
                 "$ref": "#/definitions/item"
               }
+            }
+          },
+          "default": {
+            "description": "generic error response",
+            "schema": {
+              "$ref": "#/definitions/error"
+            }
+          }
+        }
+      },
+      "post": {
+        "tags": [
+          "todos"
+        ],
+        "operationId": "addOne",
+        "parameters": [
+          {
+            "name": "body",
+            "in": "body",
+            "schema": {
+              "$ref": "#/definitions/item"
+            }
+          }
+        ],
+        "responses": {
+          "201": {
+            "description": "created",
+            "schema": {
+              "$ref": "#/definitions/item"
             }
           },
           "default": {
